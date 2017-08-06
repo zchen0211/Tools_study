b05c3a4b64a6be87fe27b6d7c2632951208feffc
zchen0211 <chenzhuoyuan07@gmail.com>
2017-08-05 17:07:33 -0700

    Makefile, CUDA added

---
+++ Makefile_study/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "functions.h"
+#include "test.h"
+
+using namespace std;
+
+int main(){
+    func();
+    print_hello();
+    cout << endl;
+    cout << "The factorial of 5 is " << factorial(5) << endl;
+    return 0;
+}
