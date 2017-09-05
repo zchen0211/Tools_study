dd26dc1df22c247d15d265a0e4ff638267757627
zchen0211 <chenzhuoyuan07@gmail.com>
2017-09-05 11:20:01 -0700

    override

--- cpp/specifiers.cc
+++ cpp/specifiers.cc
@@ -9,6 +9,19 @@ typedef
 
 constexpr
 
+// One member function overide another
+override
+struct A {
+  virtual void foo(); // must be virtual to be overriden
+  void bar();
+}
+struct B : A {
+  void foo() const override; // Error: B::foo does not override A::foo
+                               // (signature mismatch)
+  void foo() override; // OK: B::foo overrides A::foo
+  void bar() override; // Error: A::bar is not virtual
+};
+
 static_cast
 
 friend
